0af095264e88fc3a266aaf8d24bf6929ba79d893
Gonçalo Marques <goncalo.cool@gmail.com>
2018-09-30 19:26:09 +0100

    Add Vec2 and Vec3 classes, also a couple more functions

--- CGJProject/CGJProject/src/Math/Private/Vec.cpp
+++ CGJProject/CGJProject/src/Math/Private/Vec.cpp
@@ -1,6 +1,188 @@
 #include "../Public/Vec.h"
 #include <sstream>
 
+
+/////////
+// Vec2
+/////////
+
+Vec2::Vec2() :
+	x(0), y(0)
+{
+}
+Vec2::Vec2(float newXY) :
+	x(newXY), y(newXY)
+{
+}
+Vec2::Vec2(float newX, float newY) :
+	x(newX), y(newY)
+{
+}
+
+Vec2::Vec2(const Vec3 V) :
+	x(V.x), y(V.y)
+{
+}
+
+Vec2::Vec2(const Vec4 V) :
+	x(V.x), y(V.y)
+{
+}
+
+float* Vec2::GetData()
+{
+	float *arr = new float[2];
+
+	arr[0] = x;
+	arr[1] = y;
+	return arr;
+}
+
+const Vec2 operator+(const Vec2& V1, const Vec2& V2)
+{
+	Vec2 newVec = Vec2();
+
+	newVec.x = V1.x + V2.x;
+	newVec.y = V1.y + V2.y;
+
+	return newVec;
+}
+const Vec2& operator+=(Vec2& V1, const Vec2& V2)
+{
+	V1.x += V2.x;
+	V1.y += V2.y;
+	return V1;
+}
+const Vec2 operator-(const Vec2& V1, const Vec2& V2)
+{
+	Vec2 newVec = Vec2();
+
+	newVec.x = V1.x - V2.x;
+	newVec.y = V1.y - V2.y;
+	return newVec;
+}
+const Vec2& operator-=(Vec2& V1, const Vec2& V2)
+{
+	V1.x -= V2.x;
+	V1.y -= V2.y;
+	return V1;
+}
+const Vec2 operator*(const Vec2& V1, const Vec2& V2)
+{
+	Vec2 newVec = Vec2();
+
+	newVec.x = V1.x * V2.x;
+	newVec.y = V1.y * V2.y;
+	return newVec;
+}
+const Vec2& operator*=(Vec2& V1, const Vec2& V2)
+{
+	V1.x *= V2.x;
+	V1.y *= V2.y;
+	return V1;
+}
+const Vec2 operator/(const Vec2& V1, const Vec2& V2)
+{
+	Vec2 newVec = Vec2();
+
+	newVec.x = V1.x / V2.x;
+	newVec.y = V1.y / V2.y;
+	return newVec;
+}
+const Vec2& operator /=(Vec2& V1, const Vec2& V2)
+{
+	V1.x /= V2.x;
+	V1.y /= V2.y;
+	return V1;
+}
+const Vec2 operator*(const Vec2& V, const float& F)
+{
+	Vec2 newVec = Vec2();
+	Vec2 floatVec = Vec2(F);
+
+	newVec.x = V.x * floatVec.x;
+	newVec.y = V.y * floatVec.y;
+	return newVec;
+}
+const Vec2 operator*(const float& F, const Vec2& V)
+{
+	Vec2 newVec = Vec2();
+	Vec2 floatVec = Vec2(F);
+
+	newVec.x = V.x * floatVec.x;
+	newVec.y = V.y * floatVec.y;
+	return newVec;
+}
+const Vec2& operator*=(Vec2& V, float& F)
+{
+	Vec2 floatVec = Vec2(F);
+
+	V.x *= floatVec.x;
+	V.y *= floatVec.y;
+	return V;
+}
+std::ostream & operator<<(std::ostream & os, const Vec2 & V)
+{
+	os << V.ToString();
+	return os;
+}
+
+std::istream & operator>>(std::istream & is, Vec2 & V)
+{
+	is >> V.x >> V.y;
+	return is;
+}
+
+std::string Vec2::ToString() const
+{
+	std::ostringstream oss;
+	oss << "(" << x << "," << y << ")" << std::endl;
+	return oss.str();
+}
+
+float Vec2::Length() const
+{
+	return std::sqrt(x*x + y * y);
+}
+
+void Vec2::Normalize()
+{
+	float len = Length();
+	x /= len;
+	y /= len;
+}
+
+Vec2 Vec2::GetNormalized() const
+{
+	Vec2 normalized = Vec2();
+	float len = Length();
+
+	normalized.x = x / len;
+	normalized.y = y / len;
+	return normalized;
+}
+
+float Vec2::Dot(const Vec2 & V)
+{
+	return (x*V.x + y*V.y);
+}
+
+Vec2 Vec2::Cross(const Vec2 & V)
+{
+	Vec2 crossed = Vec2();
+
+	//TODO: É possível????
+	/*crossed.x = (y*V.z - z * V.y);
+	crossed.y = (z*V.x - x * V.z);
+	crossed.z = (x*V.y - y * V.x);*/
+
+	return crossed;
+}
+
+/////////
+// Vec3
+/////////
+
 Vec3::Vec3(): 
 	x(0), y(0), z(0)
 {
@@ -11,6 +193,16 @@ Vec3::Vec3(float newXYZ) :
 }
 Vec3::Vec3(float newX, float newY, float newZ) :
 	x(newX), y(newY), z(newZ)
+{
+}
+
+Vec3::Vec3(const Vec2 V) :
+	x(V.x), y(V.y), z(0)
+{
+}
+
+Vec3::Vec3(const Vec4 V) :
+	x(V.x), y(V.y), z(V.z)
 {
 }
 
@@ -34,7 +226,7 @@ const Vec3 operator+(const Vec3& V1, const Vec3& V2)
 
 	return newVec;
 }
-const Vec3& operator+=(Vec3& V1, Vec3& V2)
+const Vec3& operator+=(Vec3& V1, const Vec3& V2)
 {
 	V1.x += V2.x;
 	V1.y += V2.y;
@@ -50,7 +242,7 @@ const Vec3 operator-(const Vec3& V1, const Vec3& V2)
 	newVec.z = V1.z - V2.z;
 	return newVec;
 }
-const Vec3& operator-=(Vec3& V1, Vec3& V2)
+const Vec3& operator-=(Vec3& V1, const Vec3& V2)
 {
 	V1.x -= V2.x;
 	V1.y -= V2.y;
@@ -66,7 +258,7 @@ const Vec3 operator*(const Vec3& V1, const Vec3& V2)
 	newVec.z = V1.z * V2.z;
 	return newVec;
 }
-const Vec3& operator*=(Vec3& V1, Vec3& V2)
+const Vec3& operator*=(Vec3& V1, const Vec3& V2)
 {
 	V1.x *= V2.x;
 	V1.y *= V2.y;
@@ -82,7 +274,7 @@ const Vec3 operator/(const Vec3& V1, const Vec3& V2)
 	newVec.z = V1.z / V2.z;
 	return newVec;
 }
-const Vec3& operator /=(Vec3& V1, Vec3& V2)
+const Vec3& operator /=(Vec3& V1, const Vec3& V2)
 {
 	V1.x /= V2.x;
 	V1.y /= V2.y;
@@ -122,6 +314,12 @@ std::ostream & operator<<(std::ostream & os, const Vec3 & V)
 {
 	os << V.ToString();
 	return os;
+}
+
+std::istream & operator>>(std::istream & is, Vec3 & V)
+{
+	is >> V.x >> V.y >> V.z;
+	return is;
 }
 
 std::string Vec3::ToString() const
@@ -167,6 +365,211 @@ Vec3 Vec3::Cross(const Vec3 & V)
 	crossed.x = (y*V.z - z*V.y);
 	crossed.y = (z*V.x - x*V.z);
 	crossed.z = (x*V.y - y*V.x);
+
+	return crossed;
+}
+
+
+/////////
+// Vec3
+/////////
+
+Vec4::Vec4() :
+	x(0), y(0), z(0), w(0)
+{
+}
+Vec4::Vec4(float newXYZW) :
+	x(newXYZW), y(newXYZW), z(newXYZW), w(newXYZW)
+{
+}
+Vec4::Vec4(float newX, float newY, float newZ, float newW) :
+	x(newX), y(newY), z(newZ), w(newW)
+{
+}
+
+Vec4::Vec4(const Vec2 V) :
+	x(V.x), y(V.x), z(0), w(0)
+{
+}
+
+Vec4::Vec4(const Vec3 V) :
+	x(V.x), y(V.y), z(V.z), w(0)
+{
+}
+
+float* Vec4::GetData()
+{
+	float *arr = new float[4];
+
+	arr[0] = x;
+	arr[1] = y;
+	arr[2] = z;
+	arr[3] = w;
+	return arr;
+}
+
+const Vec4 operator+(const Vec4& V1, const Vec4& V2)
+{
+	Vec4 newVec = Vec4();
+
+	newVec.x = V1.x + V2.x;
+	newVec.y = V1.y + V2.y;
+	newVec.z = V1.z + V2.z;
+	newVec.w = V1.w + V2.w;
+	return newVec;
+}
+const Vec4& operator+=(Vec4& V1, const Vec4& V2)
+{
+	V1.x += V2.x;
+	V1.y += V2.y;
+	V1.z += V2.z;
+	V1.w += V2.w;
+	return V1;
+}
+const Vec4 operator-(const Vec4& V1, const Vec4& V2)
+{
+	Vec4 newVec = Vec4();
+
+	newVec.x = V1.x - V2.x;
+	newVec.y = V1.y - V2.y;
+	newVec.z = V1.z - V2.z;
+	newVec.w = V1.w - V2.w;
+	return newVec;
+}
+const Vec4& operator-=(Vec4& V1, const Vec4& V2)
+{
+	V1.x -= V2.x;
+	V1.y -= V2.y;
+	V1.z -= V2.z;
+	V1.w -= V2.w;
+	return V1;
+}
+const Vec4 operator*(const Vec4& V1, const Vec4& V2)
+{
+	Vec4 newVec = Vec4();
+
+	newVec.x = V1.x * V2.x;
+	newVec.y = V1.y * V2.y;
+	newVec.z = V1.z * V2.z;
+	newVec.w = V1.w * V2.w;
+	return newVec;
+}
+const Vec4& operator*=(Vec4& V1, const Vec4& V2)
+{
+	V1.x *= V2.x;
+	V1.y *= V2.y;
+	V1.z *= V2.z;
+	V1.w *= V2.w;
+	return V1;
+}
+const Vec4 operator/(const Vec4& V1, const Vec4& V2)
+{
+	Vec4 newVec = Vec4();
+
+	newVec.x = V1.x / V2.x;
+	newVec.y = V1.y / V2.y;
+	newVec.z = V1.z / V2.z;
+	newVec.w = V1.w / V2.w;
+	return newVec;
+}
+const Vec4& operator /=(Vec4& V1, const Vec4& V2)
+{
+	V1.x /= V2.x;
+	V1.y /= V2.y;
+	V1.z /= V2.z;
+	V1.w /= V2.w;
+	return V1;
+}
+const Vec4 operator*(const Vec4& V, const float& F)
+{
+	Vec4 newVec = Vec4();
+	Vec4 floatVec = Vec4(F);
+
+	newVec.x = V.x * floatVec.x;
+	newVec.y = V.y * floatVec.y;
+	newVec.z = V.z * floatVec.z;
+	newVec.w = V.w * floatVec.w;
+	return newVec;
+}
+const Vec4 operator*(const float& F, const Vec4& V)
+{
+	Vec4 newVec = Vec4();
+	Vec4 floatVec = Vec4(F);
+
+	newVec.x = V.x * floatVec.x;
+	newVec.y = V.y * floatVec.y;
+	newVec.z = V.z * floatVec.z;
+	newVec.w = V.w * floatVec.w;
+	return newVec;
+}
+const Vec4& operator*=(Vec4& V, float& F)
+{
+	Vec4 floatVec = Vec4(F);
+
+	V.x *= floatVec.x;
+	V.y *= floatVec.y;
+	V.z *= floatVec.z;
+	V.w *= floatVec.w;
+	return V;
+}
+std::ostream & operator<<(std::ostream & os, const Vec4 & V)
+{
+	os << V.ToString();
+	return os;
+}
+
+std::istream & operator>>(std::istream & is, Vec4 & V)
+{
+	is >> V.x >> V.y >> V.z >> V.w;
+	return is;
+}
+
+std::string Vec4::ToString() const
+{
+	std::ostringstream oss;
+	oss << "(" << x << "," << y << "," << z << "," << w << ")" << std::endl;
+	return oss.str();
+}
+
+float Vec4::Length() const
+{
+	return std::sqrt(x*x + y*y + z*z + w*w);
+}
+
+void Vec4::Normalize()
+{
+	float len = Length();
+	x /= len;
+	y /= len;
+	z /= len;
+	w /= len;
+}
+
+Vec4 Vec4::GetNormalized() const
+{
+	Vec4 normalized = Vec4();
+	float len = Length();
+
+	normalized.x = x/len;
+	normalized.y = y/len;
+	normalized.z = z/len;
+	normalized.w = w/len;
+	return normalized;
+}
+
+float Vec4::Dot(const Vec4 & V)
+{
+	return (x*V.x + y*V.y + z*V.z + w*V.w);
+}
+
+Vec4 Vec4::Cross(const Vec4 & V)
+{
+	Vec4 crossed = Vec4();
+
+	//TODO: Como se faz???
+	/*crossed.x = (y*V.z - z * V.y);
+	crossed.y = (z*V.x - x * V.z);
+	crossed.z = (x*V.y - y * V.x);*/
 
 	return crossed;
 }
--- CGJProject/CGJProject/src/Math/Public/Vec.h
+++ CGJProject/CGJProject/src/Math/Public/Vec.h
@@ -3,6 +3,41 @@
 #include <string.h>
 #include <math.h>
 
+class Vec2
+{
+public:
+
+	float x, y;
+
+	Vec2();
+	Vec2(float newXY);
+	Vec2(float newX, float newY);
+	Vec2(const class Vec3 V);
+	Vec2(const class Vec4 V);
+
+	friend const Vec2 operator+(const Vec2& V1, const Vec2& V2);
+	friend const Vec2& operator+=(Vec2& V1, const Vec2& V2);
+	friend const Vec2 operator-(const Vec2& V1, const Vec2& V2);
+	friend const Vec2& operator-=(Vec2& V1, const Vec2& V2);
+	friend const Vec2 operator*(const Vec2& V1, const Vec2& V2);
+	friend const Vec2& operator*=(Vec2& V1, const Vec2& V2);
+	friend const Vec2 operator/(const Vec2& V1, const Vec2& V2);
+	friend const Vec2& operator/=(Vec2& V1, const Vec2& V2);
+	friend const Vec2 operator*(const Vec2& V, const float& F);
+	friend const Vec2 operator*(const float& F, const Vec2& V);
+	friend const Vec2 operator*=(const Vec2& V, const float& F);
+	friend std::ostream& operator<<(std::ostream& os, const Vec2& V);
+	friend std::istream& operator>>(std::istream& is, Vec2& V);
+
+	float* GetData();
+	std::string ToString() const;
+	float Length() const;
+	void Normalize();
+	Vec2 GetNormalized() const;
+	float Dot(const Vec2& V);
+	Vec2 Cross(const Vec2& V);
+};
+
 class Vec3
 {
 	public:
@@ -12,19 +47,22 @@ class Vec3
 		Vec3();
 		Vec3(float newXYZ);
 		Vec3(float newX, float newY, float newZ);
+		Vec3(const class Vec2 V);
+		Vec3(const class Vec4 V);
 
 		friend const Vec3 operator+(const Vec3& V1, const Vec3& V2);
-		friend const Vec3& operator+=(Vec3& V1, Vec3& V2);
+		friend const Vec3& operator+=(Vec3& V1, const Vec3& V2);
 		friend const Vec3 operator-(const Vec3& V1, const Vec3& V2);
-		friend const Vec3& operator-=(Vec3& V1, Vec3& V2);
+		friend const Vec3& operator-=(Vec3& V1, const Vec3& V2);
 		friend const Vec3 operator*(const Vec3& V1, const Vec3& V2);
-		friend const Vec3& operator*=(Vec3& V1, Vec3& V2);
+		friend const Vec3& operator*=(Vec3& V1, const Vec3& V2);
 		friend const Vec3 operator/(const Vec3& V1, const Vec3& V2);
-		friend const Vec3& operator/=(Vec3& V1, Vec3& V2);
+		friend const Vec3& operator/=(Vec3& V1, const Vec3& V2);
 		friend const Vec3 operator*(const Vec3& V, const float& F);
 		friend const Vec3 operator*(const float& F, const Vec3& V);
 		friend const Vec3 operator*=(const Vec3& V, const float& F);
 		friend std::ostream& operator<<(std::ostream& os, const Vec3& V);
+		friend std::istream& operator>>(std::istream& is, Vec3& V);
 
 		float* GetData();
 		std::string ToString() const;
@@ -33,4 +71,39 @@ class Vec3
 		Vec3 GetNormalized() const;
 		float Dot(const Vec3& V);
 		Vec3 Cross(const Vec3& V);
+};
+
+class Vec4
+{
+public:
+
+	float x, y, z, w;
+
+	Vec4();
+	Vec4(float newXYZW);
+	Vec4(float newX, float newY, float newZ, float newW);
+	Vec4(const class Vec2 V);
+	Vec4(const class Vec3 V);
+
+	friend const Vec4 operator+(const Vec4& V1, const Vec4& V2);
+	friend const Vec4& operator+=(Vec4& V1, const Vec4& V2);
+	friend const Vec4 operator-(const Vec4& V1, const Vec4& V2);
+	friend const Vec4& operator-=(Vec4& V1, const Vec4& V2);
+	friend const Vec4 operator*(const Vec4& V1, const Vec4& V2);
+	friend const Vec4& operator*=(Vec4& V1, const Vec4& V2);
+	friend const Vec4 operator/(const Vec4& V1, const Vec4& V2);
+	friend const Vec4& operator/=(Vec4& V1, const Vec4& V2);
+	friend const Vec4 operator*(const Vec4& V, const float& F);
+	friend const Vec4 operator*(const float& F, const Vec4& V);
+	friend const Vec4 operator*=(const Vec4& V, const float& F);
+	friend std::ostream& operator<<(std::ostream& os, const Vec4& V);
+	friend std::istream& operator>>(std::istream& is, Vec4& V);
+
+	float* GetData();
+	std::string ToString() const;
+	float Length() const;
+	void Normalize();
+	Vec4 GetNormalized() const;
+	float Dot(const Vec4& V);
+	Vec4 Cross(const Vec4& V);
 };
