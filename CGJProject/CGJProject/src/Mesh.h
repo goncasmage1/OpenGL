5dfde7efdb6238edef32a14b5d20e60efec1fe5d
Pmsmm <pedro8745@gmail.com>
2019-01-06 06:17:27 +0000

    Normal Mapping Improvements
    Almost there :(

--- CGJProject/CGJProject/src/Mesh.h
+++ CGJProject/CGJProject/src/Mesh.h
@@ -14,7 +14,7 @@ public:
 
 	int VerticesPerFace = 0;
 	bool TexcoordsLoaded, NormalsLoaded;
-	std::vector<struct Vec3> Vertices, vertexData, Normals, normalData;
+	std::vector<struct Vec3> Vertices, vertexData, Normals, normalData, tangentData, biTangentData;
 	std::vector<struct Vec2> Texcoords, texcoordData;
 	std::vector<unsigned int> vertexIdx, texcoordIdx, normalIdx;
 
